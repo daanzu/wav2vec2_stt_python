04aaf8777eb537f2a154e42e9f149639fe5beba2
daanzu <daanzu@gmail.com>
2021-07-28 09:05:20 -0400

    cli: add decoding of multiple files at once

--- wav2vec2_stt/__main__.py
+++ wav2vec2_stt/__main__.py
@@ -11,17 +11,18 @@ from . import _name, Wav2Vec2STT
 def main():
     parser = argparse.ArgumentParser(prog='python -m %s' % _name)
     subparsers = parser.add_subparsers(dest='command', help='sub-command')
-    decode_parser = subparsers.add_parser('decode', help='decode a WAV file')
+    decode_parser = subparsers.add_parser('decode', help='decode one or more WAV files')
     decode_parser.add_argument('model', help='Model file to use')
-    decode_parser.add_argument('wav_file', help='WAV file to decode')
+    decode_parser.add_argument('wav_file', nargs='+', help='WAV file to decode')
     args = parser.parse_args()
 
     if args.command == 'decode':
         import wave
         wav2vec2_stt = Wav2Vec2STT(args.model)
-        wave_file = wave.open(args.wav_file, 'rb')
-        data = wave_file.readframes(wave_file.getnframes())
-        print(wav2vec2_stt.decode(data))
+        for wave_file_path in args.wav_file:
+            with wave.open(wave_file_path, 'rb') as wav_file:
+                wav_data = wav_file.readframes(wav_file.getnframes())
+                print(wav2vec2_stt.decode(wav_data))
 
     else:
         parser.print_help()
